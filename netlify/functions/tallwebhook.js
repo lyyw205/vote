325cf0405ad0f010a267ac301375041ecf0bf87e
Lyyw205 <lyyw205@gmail.com>
2025-07-11 19:07:19 +0900

    ff

--- netlify/functions/tallwebhook.js
+++ netlify/functions/tallwebhook.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 // 환경변수에서 불러오기
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_KEY
 );
 
 // Tally Webhook → POST JSON 호출
